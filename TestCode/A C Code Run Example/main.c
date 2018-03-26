4fa39cea52c62ad9fd26caa68d1e6691a1c3048a
Hasan Tarek <mdhasantarek1996@gmail.com>
2018-03-26 20:03:42 +0600

    C code run

---
+++ TestCode/A C Code Run Example/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+			//infinite loop
+			int main(){
+			
+			int i=0;
+
+			for(i=0;;i++){
+				printf("hello\n");
+
+			}
+		}
---
+++ TestCode/A C Code Run Example/test.php
@@ -0,0 +1,7 @@
+<?php
+$str = "Hello World!	
+fas
+sad\tgggg 		\n	 ggaad";
+echo $str . "<br>";
+echo trim($str);
+?>
