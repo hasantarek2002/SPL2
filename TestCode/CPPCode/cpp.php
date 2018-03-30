7ef338fb50d58fb7156043f387f0cf43fa17c0b3
Hasan Tarek <mdhasantarek1996@gmail.com>
2018-03-30 21:23:32 +0600

    A CPP code run

---
+++ TestCode/CPPCode/cpp.php
@@ -0,0 +1,103 @@
+<?php
+	
+	$CC="g++";
+	$out="timeout 5s ./a.out";
+	$code=$_POST["cppCode"];
+	
+	$input='2 3';
+	$cppCodeFile="main.cpp";
+	$inputFile="input.txt";
+	$filename_error="error.txt";
+	$executable="a.out";
+	$command=$CC." -lm ".$cppCodeFile;	
+	$command_error=$command." 2>".$filename_error;
+	$check=0;
+	
+	$file_code=fopen($cppCodeFile,"w+");
+	fwrite($file_code,$code);
+	fclose($file_code);
+	
+	$file_in=fopen($inputFile,"w+");
+	fwrite($file_in,$input);
+	fclose($file_in);
+	
+	exec("chmod 777 $executable"); 
+	exec("chmod 777 $filename_error");
+	
+	shell_exec($command_error);
+	
+	$error=file_get_contents($filename_error);
+	
+	
+	$executionStartTime = microtime(true);
+	
+	if(trim($error)=="")
+	{
+		if(trim($input)=="")
+		{
+			//$out=$out." > output.txt";
+			$output=shell_exec($out);
+		}
+		else
+		{
+			$out=$out." < ".$inputFile;
+			//$out=$out." > output.txt";
+			$output=shell_exec($out);
+		}
+		
+		echo $output .'<br>'.'first if condition'.'<br>';
+	}
+	else if(!strpos($error,"error"))
+	{
+		echo "<pre>$error</pre>";
+		if(trim($input)=="")
+		{
+			$output=shell_exec($out);
+		}
+		else
+		{
+			$out=$out." < ".$inputFile;
+			$output=shell_exec($out);
+		}
+		echo $output .'<br>'.'second if condition'.'<br>';
+	}
+		else
+	{
+		echo "<pre>$error</pre>". ' occurs in last else condition (compilation error)';
+		$check=1;
+	}
+	
+	$executionEndTime = microtime(true);
+	$seconds = $executionEndTime - $executionStartTime;
+	$seconds = sprintf('%0.2f', $seconds);
+
+
+	if($check==1)
+	{
+		echo "<pre>Compilation error</pre>";
+	}
+	else if($check==0 && $seconds>3)
+	{
+		echo "<pre>Time limit exceed</pre>";
+	}
+	else if(trim($output)=="")
+	{
+		echo "<pre>Run Time Error</pre>";
+	}
+	else if($check==0)
+	{
+		echo "<pre>Verdict : AC</pre>";
+	}
+	exec("rm $cppCodeFile");
+	exec("rm *.o");
+	exec("rm *.txt");
+	exec("rm $executable");
+	echo "<pre>Compiled And Executed In: $seconds s</pre>";
+
+
+
+	
+	
+	
+	
+?>
---
+++ TestCode/CPPCode/submitPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> C Code</h2>
+
+<form name="codeSubmitForm" action="cpp.php" method="post">
+  <textarea name="cppCode" rows="20" cols="80"></textarea>
+  <br>
+  <input type="submit" value="Submit code">
+</form>
+
+</body>
+</html>
