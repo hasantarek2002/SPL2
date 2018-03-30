f93569a951f89128f8c115292530f4120ba8b82a
Hasan Tarek <mdhasantarek1996@gmail.com>
2018-03-30 21:22:57 +0600

    A c Code run

---
+++ TestCode/CCode/submitPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2> C Code</h2>
+
+<form name="codeSubmitForm" action="c.php" method="post">
+  <textarea name="CCode" rows="20" cols="80"></textarea>
+  <br>
+  <input type="submit" value="Submit code">
+</form>
+
+</body>
+</html>
