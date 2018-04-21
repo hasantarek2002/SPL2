e0b8712d2032590605a687030687561f9ae64b2f
Hasan Tarek <mdhasantarek1996@gmail.com>
2018-04-21 20:05:04 +0600

    password validation for recovery

---
+++ programmingPlatform/js/setPassword.js
@@ -0,0 +1,34 @@
+function passwordValidation(){
+
+    var password= document.forms['setPasswordForm']['password'].value;
+    var confirmPassword= document.forms['setPasswordForm']['confirmPassword'].value;
+
+    var isError = 0;
+    document.getElementById("password-alert").innerHTML="";
+    document.getElementById("confirmPassword-alert").innerHTML="";
+
+    if(password == "" || password == null){
+        document.getElementById("password-alert").innerHTML="Please fill the password field";
+        isError=1;
+    }else if(password.length > 15){
+        document.getElementById("password-alert").innerHTML="password can not be greater than 15 charecters";
+        isError=1;
+    }
+
+
+    if(confirmPassword=="" || confirmPassword==null){
+        document.getElementById("confirmPassword-alert").innerHTML="Please confirm the password";
+        isError=1;
+    }else if(confirmPassword.length > 15){
+        document.getElementById("confirmPassword-alert").innerHTML="password can not be greater than 15 charecters";
+        isError=1;
+    }else if(password != confirmPassword){
+        document.getElementById("confirmPassword-alert").innerHTML="Password didn't match";
+        isError=1;
+    }
+    if(isError == 1){
+        return false;
+    }
+    return true;
+
+}
